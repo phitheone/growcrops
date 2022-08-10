693af4452d16a6bf43c9076668f42125a0c81f2e
phitheone <102269729+phitheone@users.noreply.github.com>
2022-08-10 10:39:47 -0400

    first commit

---
+++ src/Components/Info/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Info.css";
+import infoimg from "../../img/prcs.png";
+
+const Info = () => {
+  return (
+    <div className="ISuper">
+      <img src={infoimg} />
+    </div>
+  );
+};
+
+export default Info;
